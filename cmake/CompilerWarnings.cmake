5b968e9858a9710d3a5e1bde8da0412f643bf159
Chris Lyons <17039033+chrislyons@users.noreply.github.com>
2025-10-07 23:07:27 -0400

    Fix compiler warnings include path

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,8 +1,15 @@
 # SPDX-License-Identifier: MIT
+
 function(orpheus_enable_warnings target)
   if(MSVC)
-    target_compile_options(${target} PRIVATE /W4 /permissive-)
+    target_compile_options(${target} PRIVATE
+      /W4 /WX /permissive- /Zc:preprocessor /Zc:__cplusplus /EHsc
+      /wd5105
+      /external:W0 /external:anglebrackets /experimental:external
+    )
   else()
-    target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic)
+    target_compile_options(${target} PRIVATE
+      -Wall -Wextra -Werror -Wconversion -Wsign-conversion
+    )
   endif()
 endfunction()
