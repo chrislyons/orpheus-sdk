7eaafdfd18001f1896f3cad9dc4edfb3a1ec982c
Chris Lyons <17039033+chrislyons@users.noreply.github.com>
2025-10-11 00:38:20 -0400

    fix: stabilize googletest dependency fetching

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,10 +3,16 @@ include(FetchContent)
 
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
-FetchContent_Declare(
-  googletest
-  URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
-  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-)
 
-FetchContent_MakeAvailable(googletest)
+if(EXISTS "${CMAKE_SOURCE_DIR}/third_party/googletest/CMakeLists.txt")
+  # Use vendored googletest when present
+  add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/googletest EXCLUDE_FROM_ALL)
+else()
+  FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.14.0
+    GIT_SHALLOW TRUE
+  )
+  FetchContent_MakeAvailable(googletest)
+endif()
